036b35eacf4948e576cce2d57f09dd9ce75edde0
Nabiha Nasir <nabihanasir8375@gmail.com>
2025-07-26 08:35:38 +0500

    Update print-sepaerator.py

---
+++ python/print-sepaerator.py
@@ -0,0 +1,14 @@
+# Default separator (space)
+print("Python", "is", "awesome")
+# Output: Python is awesome
+
+# Custom separator
+print("Python", "is", "awesome", sep="-")
+# Output: Python-is-awesome
+
+print("2025", "07", "25", sep="/")
+# Output: 2025/07/25
+
+# No separator (join with nothing)
+print("Hello", "World", sep="")
+# Output: HelloWorld
