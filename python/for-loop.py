bcdd9c97f9c558fb7e8e662c0c7f4245fc389c88
Nabiha Nasir <nabihanasir8375@gmail.com>
2025-07-26 08:25:03 +0500

    Update for-loop.py

---
+++ python/for-loop.py
@@ -0,0 +1,15 @@
+fruits = ["apple", "banana", "mango"]
+
+for fruit in fruits:
+    print(fruit)
+
+for i in range(1, 6):
+    print("Number:", i)
+
+numbers = [1, 4, 7, 10, 13]
+
+for num in numbers:
+    if num % 2 == 0:
+        print(num, "is even")
+    else:
+        print(num, "is odd")
