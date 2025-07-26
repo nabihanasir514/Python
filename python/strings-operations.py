951ee1506dfb96fbcb346493c9b906aa9f80e37b
Nabiha Nasir <nabihanasir8375@gmail.com>
2025-07-26 08:40:08 +0500

    Update strings-operations.py

---
+++ python/strings-operations.py
@@ -0,0 +1,15 @@
+text = "Hello Python"
+
+# Indexing and Slicing
+print(text[0])          # H
+print(text[-1])         # n
+print(text[0:5])        # Hello
+
+# Common String Methods
+print(text.lower())     # hello python
+print(text.upper())     # HELLO PYTHON
+print(text.replace("Python", "World"))  # Hello World
+print(text.split())     # ['Hello', 'Python']
+print(text.find("Py"))  # 6
+print(len(text))        # 12
+
