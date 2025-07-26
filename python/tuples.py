8aedae2e0ff03238f1160022f4b3961946e4063d
Nabiha Nasir <nabihanasir8375@gmail.com>
2025-07-26 08:42:16 +0500

    Update tuples.py

---
+++ python/tuples.py
@@ -0,0 +1,11 @@
+# Empty tuple
+empty = ()
+
+# Tuple with values
+t = (1, 2, 3, 4)
+
+# Tuple with mixed data types
+mixed = (1, "hello", 3.14)
+
+# Tuple without parentheses (implicitly)
+implicit = 5, 10, 15
