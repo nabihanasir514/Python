e00392eb9b2312b56e594a3bf2314085a308a255
Nabiha Nasir <nabihanasir8375@gmail.com>
2025-07-26 08:43:07 +0500

    Update variables.py

---
+++ python/variables.py
@@ -0,0 +1,28 @@
+# Integer variable
+x = 10
+
+# String variable
+name = "Sarah"
+
+# Float variable
+pi = 3.14
+
+# Boolean variable
+is_valid = True
+
+
+# Assigning multiple values to multiple variables
+a, b, c = 1, 2, 3
+
+# Assigning one value to multiple variables
+x = y = z = 100
+
+
+# Assigning and printing variables
+name = "Ali"
+age = 21
+height = 5.7
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
