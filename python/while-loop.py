a6e0e0e899319d6a5d9c4b019da8933ad67f4933
Nabiha Nasir <nabihanasir8375@gmail.com>
2025-07-26 08:44:00 +0500

    Update while-loop.py

---
+++ python/while-loop.py
@@ -0,0 +1,16 @@
+while condition:
+    # code to run
+
+
+i= 1
+while True:
+    print(i)
+    if i == 3:
+        break  # exits the loop
+    i += 1
+
+
+count = 1
+while count <= 5:
+    print("Count is:", count)
+    count += 1
