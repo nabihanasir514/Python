0b39c4c2723cd6acc2d9b402f79c985a7678e902
Nabiha Nasir <nabihanasir8375@gmail.com>
2025-07-26 08:20:14 +0500

    Update comments.py

---
+++ python/comments.py
@@ -0,0 +1,10 @@
+# This is a single-line comment
+x = 10  # This is an inline comment
+
+
+"""
+This is a multi-line comment.
+Technically, it's a multi-line string,
+but Python ignores it if it's not assigned.
+"""
+x = 5
