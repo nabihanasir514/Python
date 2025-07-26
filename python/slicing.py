430f204fd5c1c38451ca7611e1f7f58e763eb188
Nabiha Nasir <nabihanasir8375@gmail.com>
2025-07-26 08:39:41 +0500

    Update slicing.py

---
+++ python/slicing.py
@@ -0,0 +1,7 @@
+text = "PythonProgramming"
+
+print(text[0:6])     # Python
+print(text[6:])      # Programming
+print(text[:6])      # Python
+print(text[::2])     # Pto rgamn
+print(text[::-1])    # gnimmargorPnohtyP (reverse)
